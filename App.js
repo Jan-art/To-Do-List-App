1cf7044640617725e17aa5a9cf3fef5ddf36953b
Jan <71454686+Jan-art@users.noreply.github.com>
2022-09-08 18:26:31 +0100

    #2 Task Log-1

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import Task from './components/Task';
 
 export default function App() {
+  const [task, setTask] = useState();
+
+  const handleAddTask = () => {
+    console.log(task);
+  }
+
   return (
     <View style={styles.container}>
       
@@ -24,9 +30,9 @@ export default function App() {
         behavior={Platform.OS === "ios" ? "padding" : "height"}
         style = {styles.writeTaskWrapper}
         >
-         <TextInput style = {styles.input} placeholder = {'Write New Task'} />
+         <TextInput style = {styles.input} placeholder = {'Write new Task'} value={task} onChangeText = {text => setTask(text)}/>
 
-         <TouchableOpacity>
+         <TouchableOpacity onPress={() => handleAddTask()}>
           <View style = {styles.addWrapper}>
             <Text style = {styles.addText}>+</Text>
           </View>
